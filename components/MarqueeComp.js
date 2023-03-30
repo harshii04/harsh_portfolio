870ec13afdc677181141c82cc9ba4b6e11f3874c
harshii04 <harshvardhan.agarwal2020a@vitstudent.ac.in>
2023-03-30 10:08:14 +0530

    Fix: Added heading gradient marquee text

--- components/MarqueeComp.js
+++ components/MarqueeComp.js
@@ -1,8 +1,16 @@
 import Marquee from "react-fast-marquee";
+import { useTheme } from "next-themes";
 
 const MarqueeComp = ({ textLabel }) => {
+  const { systemTheme, theme, setTheme } = useTheme();
+
   return (
-    <Marquee gradientWidth={100} gradient={false} speed={25}>
+    <Marquee
+      gradientWidth={100}
+      gradient={true}
+      gradientColor={theme === "dark" ? [22, 22, 22] : [255, 255, 255]}
+      speed={25}
+    >
       <span className="block bg-gradient-to-r from-[#80D0F2]  via-[#5C82FF] to-[#C480FF]  bg-clip-text px-1 text-sm font-semibold text-transparent">
         {textLabel}
       </span>
